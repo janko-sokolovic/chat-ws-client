29596e967a2536a2e3293145efe166055a7949f0
Janko <jasofalcon@gmail.com>
2017-11-21 17:08:51 +0100

    refactor

--- src/components/UserList/UserList.js
+++ src/components/UserList/UserList.js
@@ -1,6 +1,15 @@
 import React, {
     Component
 } from 'react';
+import Drawer from 'material-ui/Drawer';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import MenuItem from 'material-ui/MenuItem';
+import FontIcon from 'material-ui/FontIcon';
+
+import check from './check.png';
+
+
 import './UserList.css';
 import User from '../User/User';
 
@@ -9,19 +18,28 @@ class UserList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            users: props.users
+            users: props.users,
+            open: false
         }
     }
 
-    render() {
+    handleToggle = () => { console.log(this.state.open); this.setState({ open: !this.state.open })};
 
+    render() {
         const userItems = this.state.users.map((user) =>
-            <User key={user.name.toString()} name={user.name} />
+            <MenuItem> {user.name}</MenuItem>
         );
 
         return (<div className="UserList">
-            Online users:
-            {userItems}
+        <img src={check}/>
+            <RaisedButton
+                label="Toggle Drawer"
+                onClick={this.handleToggle}
+            />
+            <Drawer width={200} open={this.state.open}>
+                <AppBar title="Users" showMenuIconButton={false} />
+                {userItems}
+            </Drawer>
         </div>
         );
     }
