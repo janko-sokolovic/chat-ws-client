988aec70fb6bbd28031485dedb4b6773bca52721
Janko <jasofalcon@gmail.com>
2017-11-24 00:24:55 +0100

    remove unused files

--- src/actions/ActionTypes.js
+++
@@ -1,3 +0,0 @@
-
-export const SEND_MESSAGE = 'SEND_MESSAGE';
-export const RECEIVE_MESSAGE = 'RECEIVE_MESSAGE';
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import messages from './MessageReducer'
-
-const reducer = messages
-
-export default reducer
