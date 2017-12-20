95a6f05f675f236fb3644eff8f565fd02b60acd4
Janko <jasofalcon@gmail.com>
2017-12-20 23:29:21 +0100

    prepare for heroku/gh pages

--- src/socket.js
+++ src/socket.js
@@ -1,15 +1,17 @@
 
 const AppConfig = {
     PROTOCOL: "ws:",
-    HOST: "//localhost",
-    PORT: "9000"
+    // TODO: change to localhost if you wish to run it locally
+    HOST: "//serene-wave-94653.herokuapp.com",
+    PORT: ":9000"
 }
 
 const Singleton = (function () {
     let instance;
 
     function createInstance() {
-        const socket = new WebSocket(AppConfig.PROTOCOL + AppConfig.HOST + ":" + AppConfig.PORT);
+        // TODO: add +  PORT if you want to run it locally
+        const socket = new WebSocket(AppConfig.PROTOCOL + AppConfig.HOST);
         return socket;
     }
 
